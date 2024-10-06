e635c4ffa892c03fc808bdae1a523abd1ed3e5d6
Mohirjon013 <mohirjon26@gmail.com>
2024-10-06 02:47:44 +0100

    commit

---
+++ src/components/Item.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { DeleteOutlined, EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+import { Avatar, Card } from 'antd';
+const { Meta } = Card;
+const Item = ({item, refreshStudent, setRefreshStudent , refreshTeachers, setRefreshTeachers}) => {
+    function handleDeleteUser(user){
+        if(user.job){
+            fetch(`http://localhost:4000/teachers/${user.id}`, {method:"DELETE",}).then(res => {
+                setRefreshTeachers(!refreshTeachers)
+            })
+        }
+        else{
+            fetch(`http://localhost:4000/students/${user.id}`, {method:"DELETE",}).then(res => {
+                setRefreshStudent(!refreshStudent)
+            })
+        }
+    }
+    
+    return (
+      <Card
+        style={{
+          width: 300,
+        }}
+        actions={[
+          <SettingOutlined key="setting" />,
+          <EditOutlined key="edit" />,
+          <DeleteOutlined onClick={() => handleDeleteUser(item)} key="edit" />
+        ]}
+      >
+        <Meta
+          avatar={<Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=8" />}
+          title={`${item.name} - ${item.surname}`}
+          description={item?.study ? item.study : item.job}
+        />
+      </Card>
+    )
+}
+export default Item;
